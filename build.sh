49e75f9f9a440a443ad328257097f00f6eab5b8a
Sean Ross-Ross <srossross@gmail.com>
2018-08-19 10:37:21 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -eou pipefail
+
+COMMAND=$1
+export GOOS=${2:-$(go env GOOS)}
+export GOARCH=${3:-$(go env GOARCH)}
+
+echo "Building ./bin/${COMMAND}-$GOOS-$GOARCH"
+go build -o "./bin/${COMMAND}-$GOOS-$GOARCH" "commands/${COMMAND}/main.go"

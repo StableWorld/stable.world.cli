f1320aeee71cebec4c06f6aef76930f4e7224f16
Sean Ross-Ross <srossross@gmail.com>
2018-08-30 07:56:57 -0700

    Update Commands to include better logging

--- commands/common/common.go
+++ commands/common/common.go
@@ -28,48 +28,6 @@ func GetExitCode(err error) int {
 	return 1
 }
 
-// // StableWorldBucket is the bucket name
-// var StableWorldBucket string
-//
-// // StableWorldCA is the path to the root cert
-// var StableWorldCA string
-//
-// // StableWorldProxyURL is the path incuding auth to the proxy server
-// var StableWorldProxyURL string
-//
-// // StableWorldURL is base url for stable.world
-// var StableWorldURL string
-
-// func setupRootCA() error {
-// 	usr, err := user.Current()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	configDir := path.Join(usr.HomeDir, ".config", "stable.world")
-// 	log.Println("StableWorldCA", StableWorldCA)
-// 	StableWorldCA = path.Join(configDir, "ca.cert")
-//
-// 	err = os.MkdirAll(configDir, 0700)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	resp, err := http.Get(fmt.Sprintf("%s/ca.cert", StableWorldURL))
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer resp.Body.Close()
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	err = ioutil.WriteFile(StableWorldCA, body, 0644)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
-
 // MakeProxyURL takes a url and a bucket an generates url with auth
 func MakeProxyURL(URL string, bucket string) string {
 	parsedURL, err := url.Parse(URL)
@@ -80,17 +38,33 @@ func MakeProxyURL(URL string, bucket string) string {
 	return parsedURL.String()
 }
 
+var logFile string
+
 // SetupLog sets the logger to the correct file
 func SetupLog() error {
-	f, err := os.OpenFile("curl.log", os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		return err
+	logFile = os.Getenv("STABLE_WORLD_LOG_FILE")
+	if logFile == "" {
+		logFile = "stable-world.log"
 	}
-	log.SetOutput(f)
+
+	if logFile != "-" {
+		f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE, 0660)
+		if err != nil {
+			return err
+		}
+		log.SetOutput(f)
+
+	}
 	log.Println("Args", os.Args)
 	return nil
 }
 
+// TeardownLogFile will remove the log file
+func TeardownLogFile() {
+	if logFile != "" && logFile != "-" {
+		os.Remove(logFile)
+	}
+}
 func URL() string {
 	StableWorldURL := os.Getenv("STABLE_WORLD_URL")
 	if StableWorldURL == "" {
